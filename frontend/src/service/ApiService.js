c4d348aecd7d9c441d445777f96fa3d722b864b4
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2025-01-06 11:54:52 +0300

    Add product management endpoints to ApiService for CRUD operations

--- frontend/src/service/ApiService.js
+++ frontend/src/service/ApiService.js
@@ -29,4 +29,54 @@ export default class ApiService{
         })
         return response.data
     }
+
+    // // // Product Endpoints
+    static async addProduct(formDetails){
+        const response = await axios.post(`${this.BASE_URL}/product/create`, formDetails, {
+            headers: {
+                ...this.getHeader(),
+                "Content-Type":"multipart/formdata"
+            }
+        })
+        return response.data
+    }
+
+    static async updateProduct(productId,formDetails){
+        const response = await axios.put(`${this.BASE_URL}/product/update/${productId}`, formDetails,{
+            headers: {
+                ...this.getHeader(),
+                "Content-Type":"multipart/form-data"
+            }
+        })
+        return response.data
+    }
+
+    static async getAllProducts(){
+        const response = await axios.get(`${this.BASE_URL}/product/get-all`)
+        return response.data
+    }
+
+    static async searchProduct(searchValue){
+        const response = await axios.get(`${this.BASE_URL}/product/search-product`, {
+            params: {searchValue}
+        })
+        return response.data;
+    }
+
+    static async getProductsByCategoryId(categoryId){
+        const response = await axios.get(`${this.BASE_URL}/product/get-products-by-category-id/${categoryId}`)
+        return response.data
+    }
+
+    static async getProductById(productId){
+        const response = await axios.get(`${this.BASE_URL}/product/get-product-by-id/${productId}`)
+        return response.data
+    }
+
+    static async deleteProduct(productId){
+        const response = await axios.delete(`${this.BASE_URL}/product/delete/${productId}`, {
+            headers: this.getHeader()
+        })
+        return response.data
+    }
 }
