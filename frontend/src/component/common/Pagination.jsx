28f601ce88986d33f5a796a61be44a840e8124d0
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2025-01-13 23:27:05 +0300

    Add Pagination component with styling for page navigation

---
+++ frontend/src/component/common/Pagination.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import "../../style/pagination.css"
+
+const Pagination = ({currentPage, totalPages, onPageChange}) => {
+    const pageNumbers = [];
+    for(let index = 1; index < totalPages; index++){
+        pageNumbers.push(index);
+
+    }
+
+    return(
+        <div className="pagination">
+            {pageNumbers.map((number) =>(
+                <button
+                key={number}
+                onClick={() => onPageChange(number)}
+                className={number === currentPage ? 'active': ''}>
+                    {number}
+                </button>
+            )) }
+        </div>
+    )
+}
+
+export default Pagination;
