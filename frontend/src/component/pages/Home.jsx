c11279021d477936fe4a246a748dd0664fa42704
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2025-01-14 23:49:58 +0300

    Add home.css for styling the Home component

--- frontend/src/component/pages/Home.jsx
+++ frontend/src/component/pages/Home.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import Pagination from '../common/Pagination';
 import ProductList from '../common/ProductList';
 import ApiService from '../../service/ApiService';
+import "../../style/home.css"
 
 const Home = () => {
     
